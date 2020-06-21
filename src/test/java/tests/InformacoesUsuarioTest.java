7d56e8d1b11995c663a111c01686ff8a2f4f40fc
vaneycksr <vaneyck23@gmail.com>
2020-06-20 22:34:24 -0300

    abrindo o browser do chrome com o selenium

--- src/test/java/tests/InformacoesUsuarioTest.java
+++ src/test/java/tests/InformacoesUsuarioTest.java
@@ -1,12 +1,27 @@
 package tests;
 
+// fazendo com que seja uma importacao estatica
+import static org.junit.Assert.*;
 import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class InformacoesUsuarioTest {
 
     @Test
     public void testAdicionarUmaInformacaoAdicionalDoUsuario(){
 
+        // definindo que vai usar o webdriver, e onde ele esta
+        System.setProperty("webdriver.chrome.driver","/usr/bin/chromedriver");
+
+        // apenas abre o navegador
+        WebDriver navegador = new ChromeDriver();
+
+        // abrindo uma pagina no browser
+        navegador.get("http://www.juliodelima.com.br/taskit");
+
+
+        assertEquals(1,1);
     }
 
 
