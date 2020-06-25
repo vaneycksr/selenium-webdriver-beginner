7d9a18f648677b5c1cf6481764813d933ab35be6
vaneycksr <vaneyck23@gmail.com>
2020-06-24 22:32:39 -0300

    interagindo com janelas javascript e configurando um tempo de espera explicito

--- src/test/java/tests/InformacoesUsuarioTest.java
+++ src/test/java/tests/InformacoesUsuarioTest.java
@@ -5,19 +5,21 @@ import static org.junit.Assert.*;
 
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.concurrent.TimeUnit;
 
 public class InformacoesUsuarioTest {
 
     private WebDriver navegador;
-
 
     @Before
     public void setUp(){
@@ -32,10 +34,6 @@ public class InformacoesUsuarioTest {
 
         // abrindo uma pagina no browser
         navegador.get("http://www.juliodelima.com.br/taskit");
-    }
-
-    @Test
-    public void testAdicionarUmaInformacaoAdicionalDoUsuario(){
 
         // clicar no link que possui o texto "Sign In"
         navegador.findElement(By.linkText("Sign in")).click();
@@ -57,6 +55,10 @@ public class InformacoesUsuarioTest {
 
         // clicar em um link que possui o texto "MORE DATA ABOUT YOU"
         navegador.findElement(By.linkText("MORE DATA ABOUT YOU")).click();
+    }
+
+    @Ignore
+    public void testAdicionarUmaInformacaoAdicionalDoUsuario(){
 
         // clicar no botao atraves do seu xpath //button[@data-target="addmoredata"]
         //navegador.findElement(By.cssSelector("button[data-target='addmoredata']")).click(); // usando cssSelector
@@ -80,8 +82,32 @@ public class InformacoesUsuarioTest {
         String mensagem = mensagemPop.getText();
         assertEquals("Your contact has been added!", mensagem);
 
+    }
+
+    @Test
+    public void testRemoverUmContatoDeUmUsuario(){
+        // o telefone que vai ser removido, ja tem que esta adicionado no sistema
+        // /following-sibling::a -> a partir do elemento anterior pega o proximo "a"
+        // clicar no elemento pelo seu xpath //span[text()="558394568210"]/following-sibling::a
+        navegador.findElement(By.xpath("//span[text()=\"558394568210\"]/following-sibling::a")).click();
+
+        // confirmar a janela javascript
+        navegador.switchTo().alert().accept();
+
+        // Validar que a mensagem apresentada foi: "Rest in peace, dear phone!"
+        WebElement mensagemPop = navegador.findElement(By.id("toast-container"));
+        String mensagem = mensagemPop.getText();
+        assertEquals("Rest in peace, dear phone!",mensagem);
+
+        // Aguardar ate 10 segundos para que a janela desapareca
+        WebDriverWait aguardar = new WebDriverWait(navegador,10);
+        aguardar.until(ExpectedConditions.stalenessOf(mensagemPop)); // espera ate q o elemento desaparece da pagina
+
+        // clicar no link com texto "Logout"
+        navegador.findElement(By.linkText("Logout")).click();
 
     }
+
 
     @After
     public void tearDown(){
